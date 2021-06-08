9f037466dce38305909e4e9041bbce61eba330d2
Elena <e.mandrovska@gmail.com>
2021-06-08 19:20:37 +0300

    2

---
+++ src/js/apiService.js
@@ -0,0 +1,4 @@
+import { BASE_URL, API_KEY } from './constants';
+import axios from 'axios';
+
+axios.defaults.baseURL = BASE_URL;
---
+++ src/js/getRefs.js
@@ -0,0 +1,10 @@
+// export default
+//     function getRefs() {
+//     return {
+//         : document.querySelector(''),
+//         : document.querySelector(''),
+//         : document.querySelector(''),
+//         : document.querySelector(''),
+//         : document.querySelector(''),
+//     }
+// }
