b440cd8fb51cccce82015defe4c574674e504bcc
SlavaGrach <grachv77@gmail.com>
2021-06-09 23:38:10 +0300

    Add list of trends films on main page

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import './sass/main.scss';
 import './js/listsStorage';
 import './js/apiService';
 import './js/apiResultsExample'
+import MoviesApi from './js/apiService';
+import movieList from './templates/movieList.hbs'
+
+const movieListRef = document.querySelector('.movie__list');
+
+const movies = new MoviesApi();
+
+movies.getTrendingMovies().then(renderTrends);
+
+function renderTrends(films) {
+    movieListRef.innerHTML =  movieList(films.results);    
+}
