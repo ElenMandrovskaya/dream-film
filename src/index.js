ada3dc334b0ad34b66968c66c25288e2ee4b1104
dima.marjan@gmail.com <dima.marjan@gmail.com>
2021-06-10 19:14:55 +0300

    add button function

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import './sass/main.scss';
 import './js/apiService';
 import './js/apiResultsExample';
 import './js/userListBuilder';
-import './js/renderMainPage'
+import './js/renderMainPage';
+import './js/openLibrary';
 import MoviesApi from './js/apiService';
 import renderMainPage from './js/renderMainPage';
 
 renderMainPage();
-
-
-
-
