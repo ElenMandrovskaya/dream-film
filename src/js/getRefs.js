e530a75a4ba903b74413e9e9675b1cf73a087be0
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-06-13 00:26:31 +0300

    Добавляет ссылки на элементы в getRefs.js

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,13 +1,18 @@
 export default
     function getRefs() {
     return {
-        watchedBtn : document.querySelector('.button-watched'),
-        queueBtn: document.querySelector('.button-queue'),
-        searchForm: document.querySelector('.search-form'),
-        movieList: document.querySelector('.section'),
-        warning: document.querySelector('.warning'),
-        myLibrary: document.querySelector('.nav-link-library'),
-        movieSection: document.querySelector('.movie__list'),
+      watchedBtn : document.querySelector('.button-watched'),
+      queueBtn: document.querySelector('.button-queue'),
+      searchForm: document.querySelector('.search-form'),
+      movieList: document.querySelector('.section'),
+      warning: document.querySelector('.warning'),
+      myLibrary: document.querySelector('.nav-link-library'),
+      movieSection: document.querySelector('.movie__list'),
+      navBtn: document.querySelector('.pagination'),
+      prevBtn: document.querySelector('.preview-button'),
+      nextBtn: document.querySelector('.next-button'),
+      pageList: document.getElementById('pagesList'),
+      studentsList: document.querySelector('.students-link'),
   };
 }
 
