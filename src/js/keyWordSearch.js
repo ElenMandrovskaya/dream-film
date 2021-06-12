c599158a12d6dcaa99f65203933270cb236d8123
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-06-12 11:58:26 +0300

    Update keyWordSearch.js

--- src/js/keyWordSearch.js
+++ src/js/keyWordSearch.js
@@ -4,36 +4,36 @@ import getRefs from './getRefs';
 
 const refs = getRefs();
 // console.log(refs.searchForm);
+// console.log(refs.movieSection);  
 
-// const movieListRef = document.querySelector('.movie__list');
-// console.log(movieListRef);    movieSection
 const moviesApi = new MoviesApi();
 
-let searchQuery = '';
 refs.searchForm.addEventListener('submit', (onSearch));
   
 function onSearch(e) {
     e.preventDefault();
-// movieListRef.innerHTML = ''; 
-//  const form = e.currentTarget;
-//  const searchQuery = form.elements.query.value;
-    moviesApi.searchQuery = e.currentTarget.elements.query.value.trim(); //поправила
+    moviesApi.searchQuery = e.currentTarget.elements.query.value.trim();
     if (!moviesApi.searchQuery) {
         return
-    } //добавила
-    refs.movieSection.innerHTML = '';  // перенесла
-    moviesApi.resetPage(); // добавила
-    refs.searchForm.reset(); // добавила
+    } 
+    // spinner.show();
+    refs.movieSection.innerHTML = ''; 
+    // refs.searchForm.reset();
+    moviesApi.resetPage(); 
   //fetch
     moviesApi.getMovies(searchQuery).then(renderMovies);
 }
 
 
 function renderMovies(movies) {
-    // if (searchQuery === ' ') {
-    //     refs.warning.classList.remove('hidden'); 
-    //     return;   
-    //  };
+    if (movies.total_results === 0) {
+        refs.warning.classList.remove('is-hidden'); 
+        return;   
+    };
+    refs.warning.classList.add('is-hidden'); 
+    refs.movieSection.innerHTML = movieListTpl(movies.results);
+ }
+   
     //     // console.log(searchQuery);
     // if (searchQuery >= 1) {
         // refs.warning.classList.add('hidden'); 
