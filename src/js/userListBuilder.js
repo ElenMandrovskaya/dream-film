f98c453b15c8564d314cc45bd414573a9cb5bde5
dima.marjan@gmail.com <dima.marjan@gmail.com>
2021-06-15 19:08:06 +0300

    Revert "fix"
    This reverts commit 1e8cc7fabcbdffb843753645535458b8bef9e9ba.

--- src/js/userListBuilder.js
+++ src/js/userListBuilder.js
@@ -7,14 +7,16 @@ export default async function getFilms(nameList) {
   const movieListRef = document.querySelector('.movie__list');
   const userListStorage = localStorage.getItem(nameList);
   if (userListStorage !== null) {
-    const userListMovies = userListStorage.split(',');
-    userListMovies.forEach(async filmId => {
-      try {
-        const film = await axios(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}&language=ru-RU`);
-        movieListRef.insertAdjacentHTML('beforeend', movieListTPL(film.data));
-      } catch {
-        err => console.log(err);
-      }
-    });
+    if (userListStorage) {
+      const userListMovies = userListStorage.split(',');
+      userListMovies.forEach(async filmId => {
+        try {
+          const film = await axios(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}&language=ru-RU`);
+          movieListRef.insertAdjacentHTML('beforeend', movieListTPL(film.data));
+        } catch {
+          err => console.log(err);
+        }
+      });
+    }
   }
 }
